821442c7b64a2166f118e358839e86c37ce4b862
Sondyka <sondyka13@gmail.com>
2018-12-11 00:30:06 +0200

    Update file.php

--- file.php
+++ file.php
@@ -1,28 +1,40 @@
 
-include("controller/file.php");
-<? 
+<?php
 
-function addfiles($idsubmenu) {
- if(is_dir( "files/".$idsubmenu)==false){
-    mkdir("files/".$idsubmenu."/", 0777);
+
+/*function addfiles() {
+ if(is_dir( "files")==false){
+    mkdir("files", 0777);
+    echo'bhjbcjhbhb';
      }
  
  
-           if (move_uploaded_file($_FILES['file']['tmp_name'], "files/".$idsubmenu."/".basename($_FILES['file']['name'])))
+           if (move_uploaded_file($_FILES['file']['tmp_name'], "files/".basename($_FILES['file']['name'])))
     {
         echo "файл успешно загружен";
-    }
-    }
+    }else
+    echo "mdlsmckl";
+    }*/
     ?>
 
-    <form class="addfiles" enctype="multipart/form-data" method="post" hidden>
+<?php
+$uploaddir = $_SERVER['DOCUMENT_ROOT'].'/Home/uploads/';
+if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploaddir . 
+	$_FILES['userfile']['name'])) {
+    print "File is valid, and was successfully uploaded.";
+} else {
+    print "There some errors!";
+}
+?>
+
+    <form class="addfiles" enctype="multipart/form-data" method="post" >
 <div class="form-inline">
-  <input type="file" name="file" id="file" class="input-file">
+  <input type="file" name="userfile" id="file" class="input-file">
   <label for="file" class="btn btn-tertiary js-labelFile">
    <i class="fas fa-download"></i>
     <span class="js-fileName">Оберіть файл</span>
   </label>
-  <input type="submit" class="btn btn-outline-dark" onclick="';addfiles($id_submenu);echo'">
+  <input type="submit" class="btn btn-outline-dark" >
 </div>
      
 </form>
